b4624151861618bc7ecee46e86f0e7a5a1c10d49
s20454userpjatk <127026669+s20454userpjatk@users.noreply.github.com>
2024-06-04 20:38:29 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,22 @@
 import streamlit as st
 import pickle
-import numpy as np
 from datetime import datetime
+import numpy as np
+import pandas as pd
 
+# Start time
 startTime = datetime.now()
 
+# Load the pre-trained model
 filename = "model.h5"
-model = pickle.load(open(filename, "rb"))
+model = pickle.load(open(filename, 'rb'))
 
-sex_d = {
-    0: "Kobieta",
-    1: "Mężczyzna"
-}
-
+# Dictionaries for labels
+sex_d = {0: "Kobieta", 1: "Mężczyzna"}
 pclass_d = {0: "Pierwsza", 1: "Druga", 2: "Trzecia"}
+embarked_d = {0: "Cherbourg", 1: "Queenstown", 2: "Southampton"}
 
-embarked_d = {0: "Cherbourg", 1: " Queenstown", 2: "Southampton"}
-
-endTime = datetime.now()
-
-
+# Main function
 def main():
     st.set_page_config(page_title="Czy przeżyłbyś katastrofę?")
     overview = st.container()
@@ -34,16 +31,25 @@ def main():
     with left:
         sex_radio = st.radio("Płeć", list(sex_d.keys()), format_func=lambda x: sex_d[x])
         pclass_radio = st.radio("Klasa", list(pclass_d.keys()), format_func=lambda x: pclass_d[x])
-        embarked_radio = st.radio("Port zaokrętowania", list(embarked_d.keys()), format_func=lambda x: embarked_d[x])
+        embarked_radio = st.radio("Port zaokrętowania", list(embarked_d.keys()), index=2, format_func=lambda x: embarked_d[x])
 
     with right:
         age_slider = st.slider("Wiek", value=50, min_value=1, max_value=100)
-        sibsp_slider = st.slider("# Liczba rodzeństwa i/lub partnera", value=0, min_value=0, max_value=8)
-        parch_slider = st.slider("Liczba rodziców i/lub dzieci", value=0, min_value=0, max_value=6)
+        sibsp_slider = st.slider("# Liczba rodzeństwa i/lub partnera", min_value=0, max_value=8)
+        parch_slider = st.slider("# Liczba rodziców i/lub dzieci", min_value=0, max_value=6)
         fare_slider = st.slider("Cena biletu", min_value=0, max_value=500, step=10)
 
-    # Kompletowanie danych
-    data = np.array([[pclass_radio, age_slider, sibsp_slider, parch_slider, fare_slider, embarked_radio, sex_radio]])
+    sex_male = 1 if sex_radio == 1 else 0
+    data = pd.DataFrame({
+        'Pclass': [pclass_radio],
+        'Age': [age_slider],
+        'SibSp': [sibsp_slider],
+        'Parch': [parch_slider],
+        'Fare': [fare_slider],
+        'Embarked': [embarked_radio],
+        'male': [sex_male]
+    })
+
     survival = model.predict(data)
     s_confidence = model.predict_proba(data)
 
